36bc2948570e53698f52b93cd3ff4b06d03d3f81
mariatomy9 <mariatomy555@gmail.com>
2022-05-06 23:57:18 +0530

    dividenconquer

---
+++ Divide n Conquer/compare_two_strings.py
@@ -0,0 +1,18 @@
+from requests import delete
+
+
+def findMinOperations(s1,s2,index1,index2):
+    if index1==len(s1):
+        return len(s2)-len(s1)
+    if index2==len(s2):
+        return len(s2)-index1
+    if s1[index1]==s2[index2]:
+        return findMinOperations(s1,s2,index1+1,index2+1)
+
+    else:
+        deleteOp=1+findMinOperations(s1,s2,index1,index2+1)
+        insertOp=1+findMinOperations(s1,s2,index1+1,index2+1)
+        replaceOp=1+findMinOperations(s1,s2,index1+1,index2+1)
+        return min(deleteOp,insertOp,replaceOp)
+
+print(findMinOperations("table","tbrles",0,0))
